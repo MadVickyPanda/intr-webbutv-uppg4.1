76b55217e99d94ce926bb6871d26e1dd91f4af5f
MadVickyPanda <solboll@hotmail.com>
2025-09-19 11:25:53 +0200

    js

--- script.js
+++ script.js
@@ -6,19 +6,49 @@ const addToDoBtn=document.querySelector("#addTodoBtn");
 const infoTextElement = document.querySelector("small");
 const todoList=document.querySelector("#todoList");
 let todoText = "";
-let completed = + 0;
+let completed = 0;
 
 addToDoBtn.addEventListener("click", addTodo);
+itemText.addEventListener("click", )
 
 function addTodo(){
     // things that will happen when you click the button
+    infoTextElement.textContent="";
 todoText = inputTodo.value;
+ if (todoText.length == 0){
+    infoTextElement.textContent="Du måste skriva något!"
+    return;
+ }
+
 const item = document.createElement('li');
 todoList.appendChild(item);
 
 const itemText = document.createElement('span');
 itemText.innerText = todoText;
+
+//add eventlistener to span with text
+itemText.addEventListener("click",
+    function(){
+        if(itemText.getAttribute('class') == "completed") {
+        itemText.setAttribute('class', "");
+        completed--;
+        completedElement.textContent= completed +" Slutförd!"
+             }
+        else 
+        {
+            itemText.setAttribute("class", "completed");
+            completed++;
+            completedElement.textContent= completed + " Sluförd!"
+        }
+    }
+
+
+)
 item.appendChild(itemText);
 
-}
 
+
+//empty input
+
+inputTodo.value="";
+}
